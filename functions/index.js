495c06af4aa940f20d85322ee17dea5b61f2edd9
enfrte <enfrte@gmail.com>
2020-03-11 09:27:52 +0200

    Removed unnecessary json formatting

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ app.use(cors({ origin: true })); // Automatically allow cross-origin requests
 
 // To test this route, add the route /test to the end of the exports url. So in this case, /api/test
 app.get('/test', (req, res) => {
-  return res.status(200).json({"message":"Hellö Wörld"});
+  return res.status(200).json({message:"Hellö Wörld"});
 });
 
 exports.api = functions.https.onRequest(app);
